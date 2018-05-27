23a1ac2a4447a9dbb484903d6b1e36d222638c34
Askhat <baltabaev.a2509@gmail.com>
2018-05-27 17:21:57 +0600

    Reafy

--- client/src/container/Chat.js
+++ client/src/container/Chat.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
-import { Input, Button } from 'react-materialize'
+import { Input, Button, Preloader } from 'react-materialize'
 import './Chat.css'
+import axios from 'axios'
 
 class Chat extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			content: '',
+			show: false,
+			isSpam: null
+		}
+	}
+	onSubmit(){
+		this.setState({show: true, isSpam: null})
+		axios.post('/predict', {"mail": this.state.content}).then(res => {
+			this.setState({isSpam: res.data})
+		})
+	}
 	render(){
 		return(
 			<div className='chat'>
-				<Input type='textarea' />
-				<Button waves='light'>Send</Button>
+				<Input type='textarea' onChange={(event) => this.setState({content: event.target.value})}/>
+				<Button waves='light' onClick={() => this.onSubmit()}>Send</Button>
+				<div className='response'>
+					{
+						this.state.show ? 
+							this.state.isSpam ? <div>{
+								this.state.isSpam === 'ham' ?
+								<div className='result'>
+									<div className="image">
+										<img src="/assets/success.jpg" />
+									</div>
+									<div className="result-text">
+										HAM!
+									</div>
+								</div>
+								: this.state.isSpam === 'spam' ? 
+								<div className="result">
+									<div className="image">
+										<img src="/assets/reject.jpg" />
+									</div> 
+									<div className="result-text">
+										SPAM!
+									</div>
+								</div>		
+								: null
+							}</div> : <Preloader flashing/>
+						: null
+					}
+				</div>
 			</div>
 		)
 	}
